ab3eb66b18f5c8d2adc69b011f956592fc010f04
sameerasad <ssameerkhan525@gmail.com>
2020-12-20 19:02:54 +0500

    add dynamic port using environment variable PORT

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
 app.get("/course/api", (req, res) => {
   res.send([1, 2, 3, 4]);
 });
-//set PORT=5000
+//export PORT=5000;
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`app is listening on ${port}`));
