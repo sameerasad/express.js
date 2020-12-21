ea70dbe512bde0a42925c46fe537b9dc0f5a5f75
sameerasad <ssameerkhan525@gmail.com>
2020-12-21 15:28:11 +0500

    handle the http get request

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "courses1" },
+  { id: 2, name: "courses2" },
+  { id: 3, name: "courses3" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
 });
 
 app.get("/course/api", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
-app.get("/course/api/post/:year/:months", (req, res) => {
-  res.send(req.params);
+app.get("/course/api/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res
+      .status(404)
+      .send("you entered item is not here please search by right id!");
+  res.send(course);
 });
+
 //export PORT=5000;
 
 const port = process.env.PORT || 3000;
