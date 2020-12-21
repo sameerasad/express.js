ff3f6e2d6b7237976ce1c17ca7bde58ca25469d4
sameerasad <ssameerkhan525@gmail.com>
2020-12-21 17:43:40 +0500

    http post method

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const app = express();
+const Joi = require("joi");
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "courses1" },
   { id: 2, name: "courses2" },
   { id: 3, name: "courses3" },
+  { id: 4, name: "courses4" },
 ];
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
 });
 
-app.get("/course/api", (req, res) => {
+app.get("/courses/api", (req, res) => {
   res.send(courses);
 });
-app.get("/course/api/:id", (req, res) => {
+
+app.post("/courses/api", (res, req) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+  res.send(course);
+});
+
+app.get("/courses/api/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
     res
