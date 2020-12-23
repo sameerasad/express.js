8101b18b43784c5a0a85f9565813fc8de1504f7a
sameerasad <ssameerkhan525@gmail.com>
2020-12-23 11:21:31 +0500

    post API FIXED all issues

--- app.js
+++ app.js
@@ -18,62 +18,21 @@ app.get("/", (req, res) => {
 app.get("/courses/api", (req, res) => {
   res.send(courses);
 });
-
-app.post("/courses/api", (res, req) => {
-  const schema = {
-    name: Joi.string().min().required(),
-  };
-  const result = joi.validate(req.body, schema);
-  console.log(result);
-  if (result.error) {
-    res.status(400).send(result.error.detail[0].message);
-    return;
-  }
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-
-  app.put("/courses/api/:id", (req, res) => {
-    /*
-    1)look up the course
-    2)if not exist return status(404)
-    3)validate the course
-    4)if not valid return (400)means bad request
-    5)if valid
-       update the course
-       return the course in response*/
-
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course)
-      res
-        .status(404)
-        .send("entered item is not here please search by right id!");
-
-    const schema = {
-      name: Joi.string().min().required(),
-    };
-    const result = joi.validate(req.body, schema);
-    console.log(result);
-    if (result.error) {
-      res.status(400).send(result.error.detail[0].message);
-      return;
-    }
-
-    course.name = req.body.name;
-    res.send(course);
-  });
-
-  courses.push(course);
-  res.send(course);
-});
-
 app.get("/courses/api/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
     res
       .status(404)
       .send("you entered item is not here please search by right id!");
+  res.send(course);
+});
+
+app.post("/courses/api", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
   res.send(course);
 });
 
